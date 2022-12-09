3138b45fe5fc4b1b660a3b2c7a20065bc8aa6b1d
luiseufrasio <luis.eufrasio@gmail.com>
2022-12-09 08:36:45 -0300

    FISH-6475 : transforming files

--- src/main/java/fish/payara/micro/actions/MigrateToJakartaEE10Action.java
+++ src/main/java/fish/payara/micro/actions/MigrateToJakartaEE10Action.java
@@ -1,5 +1,6 @@
 package fish.payara.micro.actions;
 
+import com.intellij.ide.impl.ProjectUtil;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
@@ -16,8 +17,10 @@ import fish.payara.micro.PayaraMicroProject;
 import fish.payara.micro.maven.MavenProject;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.util.logging.Logger;
 
+import static java.util.logging.Level.INFO;
 import static java.util.logging.Level.WARNING;
 
 public class MigrateToJakartaEE10Action extends MicroAction {
@@ -25,28 +28,57 @@ public class MigrateToJakartaEE10Action extends MicroAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent actionEvent) {
-        VirtualFile srcFolder = PlatformDataKeys.VIRTUAL_FILE.getData(actionEvent.getDataContext());
+        VirtualFile srcFile = PlatformDataKeys.VIRTUAL_FILE.getData(actionEvent.getDataContext());
         FileChooserDescriptor fileDescriptor = new FileChooserDescriptor(false,true,
                 false,false,false,false)
-                .withTitle("Choose the new Project Destination Folder");
-        VirtualFile destFolder = FileChooser.chooseFile(fileDescriptor, actionEvent.getProject(), srcFolder);
-        String newProjectName = srcFolder.getName() + "-JakartaEE10";
-        String destinationPath = destFolder.getPath() + "/" + newProjectName;
+                .withTitle("Choose the New Project Destination Folder");
+        VirtualFile destFolder = FileChooser.chooseFile(fileDescriptor, actionEvent.getProject(), srcFile);
+        assert srcFile != null;
+        String destinationPath = getDestinationPath(destFolder, srcFile);
 
         final Project project = CommonDataKeys.PROJECT.getData(actionEvent.getDataContext());
+        assert project != null;
         PsiFile[] poms = FilenameIndex.getFilesByName(project, "pom.xml", GlobalSearchScope.projectScope(project));
         PayaraMicroProject microProject = new MavenProject(project, poms[0]);
         String projectName = project.getName();
         JBTerminalWidget terminal = getTerminal(project, projectName);
         if (terminal != null) {
-            executeCommand(terminal, microProject.getTransformCommand(srcFolder.getPath(), destinationPath));
+            executeCommand(terminal, microProject.getTransformCommand(srcFile.getPath(), destinationPath));
             Messages.showMessageDialog(
-                    "Generating new Project: " + newProjectName + "!",
+                    "Generating: " + destinationPath,
                     "Confirmation",
                     Messages.getInformationIcon());
         } else {
             LOG.log(WARNING, "Shell window for {0} is not available.", projectName);
         }
+        finalAction(destinationPath, project);
+    }
+
+    private static void finalAction(String destinationPath, Project project) {
+        File file = new File(destinationPath);
+        while (!file.exists()) {
+            LOG.log(INFO, "waiting for terminal transform command");
+        }
+        if (file.isFile()) {
+            Messages.showMessageDialog(
+                    "File " + destinationPath + " created!",
+                    "Finished",
+                    Messages.getInformationIcon());
+        } else {
+            ProjectUtil.openOrImport(destinationPath, project, true);
+        }
+    }
+
+    @NotNull
+    private static String getDestinationPath(VirtualFile destFolder, VirtualFile srcFile) {
+        String fileName = srcFile.getName();
+        if (srcFile.isDirectory()) {
+            return destFolder.getPath() + "/" + fileName + "-JakartaEE10";
+        }
+        int dotIndex = fileName.lastIndexOf(".");
+
+        return destFolder.getPath() + "/" + fileName.substring(0, dotIndex) + "JakartaEE10"
+                + fileName.substring(dotIndex);
     }
 
     @Override
@@ -55,8 +87,9 @@ public class MigrateToJakartaEE10Action extends MicroAction {
     @Override
     public void update(AnActionEvent e) {
         VirtualFile file = PlatformDataKeys.VIRTUAL_FILE.getData(e.getDataContext());
-        e.getPresentation().setEnabledAndVisible(
-                isRoot(file, e.getProject()));
+        e.getPresentation().setEnabledAndVisible(file != null && (
+                !file.isDirectory() || isRoot(file, e.getProject())
+        ));
     }
 
     private boolean isRoot(VirtualFile file, Project project) {
